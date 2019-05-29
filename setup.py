68266b16eda69255f0fd4165f061e8bbc6b277a8
ceccoemi <cecchini.mle@gmail.com>
2019-05-29 14:33:01 +0200

    Setup project as a pip package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+from wlkernel import __version__
+
+
+setup(
+    name='wlkernel',
+    version=__version__,
+    description='Weisfeiler-Lehman kernel for RDF graphs',
+)
---
+++ wlkernel/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '0.1'
+
+
+from ._wlkernel import *
